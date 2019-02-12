5f1a7f7f202880580dc8f022d3ceea2b0bfde967
PrzemyslawMatlega <przemekmat@yahoo.com>
2019-02-12 12:36:40 +0100

    Deploy

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -1,25 +1,20 @@
 export default class Likes {
-    constructor(likes= []) {
-        this.likes = likes;
+  constructor(likes = []) {
+    this.likes = likes;
+  }
 
-    }
+  addLike(title, img, url) {
+    const like = { title, img, url };
+    this.likes.push(like);
+  }
 
-addLike(title,img,url){
-        const like = {title,img,url};
-        this.likes.push(like);
-    
-}
+  deleteLike(url) {
+    const index = this.likes.findIndex(el => el.url === url);
 
-deleteLike(url){
+    this.likes.splice(index, 1);
+  }
 
-        const index = this.likes.findIndex(el=>el.url ===url);
-      
-        this.likes.splice(index,1);
-}
-
-isLiked(url){
-    return this.likes.findIndex(el => el.url=== url) !== -1; 
-
-}
-
+  isLiked(url) {
+    return this.likes.findIndex(el => el.url === url) !== -1;
+  }
 }
