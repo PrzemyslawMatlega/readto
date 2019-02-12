a2399ed56a636aeaeddebace57414e78e1798d63
PrzemyslawMatlega <przemekmat@yahoo.com>
2019-02-12 08:36:51 +0100

    fixes

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -7,14 +7,13 @@ export default class Likes {
 addLike(title,img,url){
         const like = {title,img,url};
         this.likes.push(like);
-        console.log(this);
-
+    
 }
 
 deleteLike(url){
 
         const index = this.likes.findIndex(el=>el.url ===url);
-        console.log(index);
+      
         this.likes.splice(index,1);
 }
 
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,12 +1,12 @@
-export const elements ={
-    search_main: document.querySelector('.search'),
-    search: document.querySelector('.search__form'),
-    searchField: document.querySelector('.search__form-field'),
-    navbuttons: document.querySelector('.content__buttons'),
-    content__main: document.querySelector('.content'),
-    content: document.querySelector('.content__main'),
-    header: document.querySelector('.header'),
-    logo: document.querySelector('.logo'),
-    sideBar: document.querySelector('.sideBar'),
-    trash : document.querySelector('.sideBar__trash')
-}
+export const elements = {
+  search_main: document.querySelector('.search'),
+  search: document.querySelector('.search__form'),
+  searchField: document.querySelector('.search__form-field'),
+  navbuttons: document.querySelector('.content__buttons'),
+  content__main: document.querySelector('.content'),
+  content: document.querySelector('.content__main'),
+  header: document.querySelector('.header'),
+  logo: document.querySelector('.logo'),
+  sideBar: document.querySelector('.sideBar'),
+  trash: document.querySelector('.sideBar__trash'),
+};
